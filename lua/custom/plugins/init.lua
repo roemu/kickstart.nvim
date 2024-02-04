95b943fc3c829b04fbfe6edecd616ab33e58acc8
Roman Suter <rsu111923@iet-gibb.ch>
2024-02-04 12:56:54 +0100

    Changed around some telescope and treesitter configs

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,7 +6,7 @@ return {
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
-    -- LSP Configuration & Plugins
+  -- LSP Configuration & Plugins
   {
     'neovim/nvim-lspconfig',
     dependencies = {
@@ -16,7 +16,7 @@ return {
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
+      { 'j-hui/fidget.nvim',       opts = {} },
 
       -- Additional lua configuration, makes nvim stuff amazing!
       'folke/neodev.nvim',
@@ -24,6 +24,36 @@ return {
   },
 
   -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim', opts = {} },
-}
+  { 'folke/which-key.nvim',  opts = {} },
+  { 'numToStr/Comment.nvim', opts = {} },
+  {
+    -- Autocompletion
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      -- Snippet Engine & its associated nvim-cmp source
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
 
+      -- Adds LSP completion capabilities
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-path',
+
+      -- Adds a number of user-friendly snippets
+      'rafamadriz/friendly-snippets',
+    },
+  },
+  {
+    -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help ibl`
+    main = 'ibl',
+    opts = {},
+  },
+  -- { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+  {
+    'folke/todo-comments.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {}
+  }
+}
